00608d3a3d6cbe5ef817e837b5efc190fb58e4e3
chamcherno <cherno@writemedia.co.uk>
2023-05-05 09:49:17 +0000

    Done

--- TimeZoneConverter/Program.cs
+++ TimeZoneConverter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Data;
 using TimeZoneConverter.Models;
 
@@ -16,15 +18,68 @@ namespace DateTimeConversion
 
                 foreach (var property in dateTimeProperties)
                 {
-                    var timeZone = TimeZoneInfo.FindSystemTimeZoneById("E. Australia Standard Time");
+
+                    var propertyName = property.Name;
+
                     var tableName = property.DeclaringEntityType.GetTableName();
+
+                    var query = $"SELECT [{propertyName}] FROM [{tableName}]";
+
+                    // Execute the query and iterate over the results
+                    using (var command = context.Database.GetDbConnection().CreateCommand())
+                    {
+                        command.CommandText = query;
+                        command.CommandType = CommandType.Text;
+
+                        context.Database.OpenConnection();
+
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // Get the datetime value from the reader
+                                var datetimeValue = reader.GetDateTime(reader.GetOrdinal(propertyName));
+
+                                string time = datetimeValue.ToString();
+                                DateTime dateTime = DateTime.Parse(time);
+                                string formattedString = dateTime.ToString("yyyy MM dd HH mm ss");
+
+                                string[] splitTimeString = formattedString.Split(' ');
+                                int[] splitTimeStringToInt = Array.ConvertAll(splitTimeString, int.Parse);
+
+                                int year = splitTimeStringToInt[0];
+                                int month = splitTimeStringToInt[1];
+                                int day = splitTimeStringToInt[2];
+                                int hour = splitTimeStringToInt[3];
+                                int minute = splitTimeStringToInt[4];
+                                int second = splitTimeStringToInt[5];
+
+                                DateTime dateAndTime = new DateTime(year, month, day, hour, minute, second);
+                                var yearValue = dateAndTime.Year;
+                                var BstStart = new DateTime(yearValue, 3, 26);
+                                var BstEnd = new DateTime(yearValue, 10, 29);
+                                var timezone_value = "GMT Standard Time";
+                                
+                                while (!(month >= BstStart.Month && day >= 26) && (month <= BstEnd.Month && day <= 29))
+                                {
+                                    TimeZoneInfo ukTime = TimeZoneInfo.FindSystemTimeZoneById(timezone_value);
+                                    dateAndTime = DateTime.SpecifyKind(dateAndTime, DateTimeKind.Unspecified);
+                                    var result = TimeZoneInfo.ConvertTimeToUtc(dateAndTime, ukTime);
+                                }
+                            }
+                        }
+                    }
+
+                    tableName = property.DeclaringEntityType.GetTableName();
                     var columnName = property.GetColumnName();
-                    var sql = $"UPDATE {tableName} SET {columnName} = DATEADD(hour, {timeZone.BaseUtcOffset.TotalHours}, {columnName})";
+                    var sql = $@"UPDATE[{tableName}] SET[{columnName}] = DATEADD(hour, -1, [{columnName}])";
                     context.Database.ExecuteSqlRaw(sql);
                 }
-                 context.SaveChanges();
+                context.SaveChanges();
             }
             Console.WriteLine("Done!");
         }
     }
 }
+
+
